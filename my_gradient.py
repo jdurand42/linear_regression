cf34e35191fd45583df4ee1bcd6ce4de7195c786
jdurand42 <jerome.d274@gmail.com>
2021-10-04 17:57:22 +0200

    no more overflow, and better code

--- my_gradient.py
+++ my_gradient.py
@@ -38,19 +38,30 @@ def openPredicator():
 	return file
 
 
+# for large data sets, due to overflow, the matrix are divided by a divisor
+# by default it's 10000
+# when writing the thetas in predicator.py, t0 is multiplied by the divisor to
+# rescale the graph
+
+# Protype: X, Y : Matrix
+# fileOpener: a fonction if you need to update the thetas in a predicator file
+# you have to write the fonction with the right name and error print
+# put anything else if you don't want to use it
+# div: A divisor to divide the sets to prevent overflow in large numbers
+# learningRate: learningRate (alpha)
 class Linear_Regression():
-	def __init__(self, X, Y, file, div = 10000, learningRate = 0.01):
-		self.t = [0, 0]
+	def __init__(self, X, Y, fileOpener, div = 1, learningRate = 0.01):
+		self.t = np.array([0, 0], dtype=np.float64)
 		self.l = learningRate
 		self.divisor = div
 		self.X = X
 		self.Y = Y
-		self.file = file
+		self.fileOpener = fileOpener
 
 	def predict(self):
 		if len(X) == 0:
 			pass
-		Y_pred = np.array([])
+		Y_pred = np.array([], dtype=np.float64)
 		for x in X:
 			Y_pred = np.append(Y_pred, self.t[1] * x + self.t[0])
 		return Y_pred
@@ -65,15 +76,15 @@ class Linear_Regression():
 		#if Y_pred == None:
 		Y_pred = self.predict()
 		m = len(Y_pred)
-		b = np.array([])
+		b = np.array([], dtype=np.float64)
 		for i in range(len(Y_pred)):
 			if self.Y[i] != 0:
 				b = np.append(b, abs(Y_pred[i] - self.Y[i]) / self.Y[i])
-		return (1 - np.sum(b)) / m
+		return ((1 - np.sum(b)) / m)
 
 	def getCost(self, Y_pred = np.array([])):
 		m = len(self.X)
-		if Y_pred == np.array([]):
+		if Y_pred == np.array([], dtype=np.float64):
 			Y_pred = self.predict()
 		j = (1 / 2 * m) * (np.sum(Y_pred - self.Y)**2)
 		return j
@@ -84,42 +95,44 @@ class Linear_Regression():
 		f.clear()
 		plt.plot(self.X * self.divisor, Y_pred * self.divisor, color="red")
 		plt.scatter(self.X * self.divisor, self.Y * self.divisor)
+		#plt.plot(self.X, Y_pred, color="red")
+		#plt.scatter(self.X, self.Y)
 		f.show()
 
+	def cleanExit(self, file):
+		print('Error: could not write thetas into predicator file', file=sys.stderr, flush=True)
+		file.closeFile()
+		exit(1)
+
 	def updateProgram(self):
-		self.file = openPredicator()
-		data = self.file.read()
+		try:
+			file = self.fileOpener()
+		except:
+			return
+		data = file.read()
 		iT0 = data.find("t0 =")
 		if iT0 == -1:
-			print('Error: could not write thetas into predicator file', file=sys.stderr, flush=True)
-			self.closeFile()
-			exit(1)
-		data = data.replace(data[iT0:data.find("\n", iT0)], 't0 = {}'.format(self.t[0]), 1)
+			self.cleanExit(file)
+		data = data.replace(data[iT0:data.find("\n", iT0)], 't0 = {}'.format(self.t[0] * self.divisor), 1)
 		iT1 = data.find("t1 =")
 		if iT1 == -1:
-			print('Error: could not write thetas into predicator file', file=sys.stderr, flush=True)
-			self.closeFile()
-			exit(1)
+			self.cleanExit(file)
 		data = data.replace(data[iT1:data.find("\n", iT1)], 't1 = {}'.format(self.t[1]), 1)
 		print(self.t[0], self.t[1])
-		self.file.seek(0)
-		self.file.write(data)
-		self.file.truncate()
-		self.closeFile()
-
-
-	def closeFile(self):
-		self.file.close()
+		file.seek(0)
+		file.write(data)
+		file.truncate()
+		file.close()
 
 div = 10000
 b = parser()
 file = openPredicator()
-X = np.array(b[0]) / div
-Y = np.array(b[1]) / div
+X = np.array(b[0], dtype=np.float64) / 10000
+Y = np.array(b[1], dtype=np.float64) / 10000
 
 steps = 100
 epochs = 0
-regression = Linear_Regression(X, Y, file, div)
+regression = Linear_Regression(X, Y, openPredicator, div)
 Y_pred = regression.predict()
 while 1:
 	Y_pred = regression.predict()
@@ -133,5 +146,4 @@ while 1:
 		print('Accuracy is: ', accuracy)
 		choice = input("Do you wish to quit? (y)/(n)?")
 		if choice == 'y':
-			regression.closeFile()
 			break
--- predicator.py
+++ predicator.py
@@ -5,28 +5,34 @@ import tkinter
 import matplotlib
 matplotlib.use('TkAgg')
 
-t0 = 0.6032888905708175
-t1 = -0.002052359791241146
+t0 = 8487.802590428824
+t1 = -0.021356199213165873
 
 def estimatePrice(x):
 	return t0 + (t1 * x)
 
 def predicate(X):
-	Y_pred = np.array([])
+	Y_pred = np.array([], dtype=np.float64)
 	for x in X:
-		Y_pred = np.append(Y_pred, estimatePrice(x))
+		Y_pred = np.append(Y_pred, (t1 * x) + t0)
 	return (Y_pred)
 
 def drawGraph(X, Y_pred):
 	f = plt.figure('estimated price')
-	plt.scatter(X, Y_pred)
+	plt.scatter(X, Y_pred, color="red")
 	plt.grid(True)
 	plt.plot(X, Y_pred)
 	plt.show()
 
+
+def printPreds(X, Y_pred):
+	for i in range(len(X)):
+		print("Km: {}: price: {}".format(X[i], Y_pred[i]))
+
 def main():
 	args = sys.argv[1:]
-	X = np.array([])
+	x = []
+	X = np.array([], dtype=np.float64)
 	if len(args) == 0:
 		print('Usage: python3 predicator.py <Miles(Km) 1> ... <Miles (km) 2>')
 		exit(0)
@@ -34,10 +40,12 @@ def main():
 		if el.isnumeric() == False:
 			print('Error: unreconized parameter: {}, only numeric values are accepted'.format(el), file=sys.stderr, flush=True)
 			exit(1)
+		x.append(int(el))
 		X = np.append(X, int(el))
-	print('Sample: ', X)
+	# print('Sample: ', X)
 	Y_pred = predicate(X)
+	print(Y_pred)
+	printPreds(x, Y_pred)
 	drawGraph(X, Y_pred)
-
 
 main()
